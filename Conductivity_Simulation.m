52eb40fb501233ae1295881ca40fb87973e5d1c9
djammy <110920619+djammy@users.noreply.github.com>
2022-09-13 16:06:38 +0100

    Update and rename code.m to Conductivity_Simulation.m

--- Conductivity_Simulation.m
+++ Conductivity_Simulation.m
@@ -1,20 +1,25 @@
 % clear all
 
-me = 9.10938215E-31;
-q = 1.60217646E-19;
-permittivity = 8.854187817620E-12;
-meff = 0.47*me;
+% Input all parameters
+me = 9.10938215E-31; % electron mass
+q = 1.60217646E-19;  % electronic charge
+permittivity = 8.854187817620E-12; % vacuum permitivitty
+
+meff = 0.47*me; % effective electron mass
+
 f = (0:0.1:10); % create frequency range: 0 - 10 THz
 f = f';
-w = f*1e12*2*pi;
-A = 1; % Dummy factor in Lorentzian Equation
-epsilon = 12.95;
-Np = 1e23; Neq = 1e14; gamma = 0.5e14; % Change these values for shape of graph
+w = f*1e12*2*pi; % omega (angular frequency)
+
+    A = 1; % Dummy factor in Lorentzian Equation
+epsilon = 12.95; % dielectric constant of sample
+
+Np = 1e23; % photoexcited carrier concentration
+Neq = 1e14; % equilibrium carrier concentration
+gamma = 0.5e14; % scattering frequency
 
 %% Choose Method
-method = 1; % SP/Lorentzian Model
-% method = 2; % Drude Model
-% method = 3; % Drude-Smith Model
+method = 1; % [SP/Lorentzian Model:1, Drude Model:2, Drude-Smith Model:3]
 
 %% Lorentzian
 if method == 1
@@ -24,15 +29,18 @@ if method == 1
     lor = @(w,w0,N,gamma,meff)((1i*N*q^2*w)./(meff*(w.^2-w0.^2+1i*w*gamma)));
     conductivity_fit = A*(lor(w,w0,Np+Neq,gamma,meff)- lor(w,w0eq,Neq,gamma,meff));
 end
+
 %% Drude Model
 if method == 2
     drude = ((1i*(2e23)*q^2*w)./(mh_eff*(w.^2+1i*w*1e13)));
 end
+
 %% Drude-Smith Model
 if method == 3
     c = -1;
     drudeSmith = ((1i*(1e20)*q^2*w)./(meff*(w.^2+1i*w*1e13))) + c*((1i*(1e20)*q^2*w)./(meff*(w.^2+1i*w*1e13)))./(1 - 1i*w*1e-13);
 end
+
 %% Plots
 % figure
 % box on
